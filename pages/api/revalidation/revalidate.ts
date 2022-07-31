c02d907bdfccf507272c86c5ed26b101d48fdde7
riad <riad@gmail.com>
2022-07-31 12:49:26 +0100

    on demand revalidation

---
+++ pages/api/revalidation/revalidate.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // Check for secret to confirm this is a valid request
+  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+
+  try {
+    // this should be the actual path not a rewritten path
+    // e.g. for "/blog/[slug]" this should be "/blog/post-1"
+    const { path } = req.query;
+    if (typeof path === 'string' && path) {
+      await res.revalidate(path);
+      return res.json({ revalidated: true });
+    }
+    return res.json({ revalidated: false, message: 'path is not valid' });
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send('Error revalidating');
+  }
+}
+ß;
