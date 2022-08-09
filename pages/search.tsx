bf412e67b1905797108ea5080c899b34ceaeaefc
riad <riad@gmail.com>
2022-08-09 09:50:00 -0400

    justify fileterBar

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,13 +1,19 @@
 import { NextPage } from 'next';
 import { useEffect, useState } from 'react';
+import ProductsList from '../common/components/productList/ProductsList';
+import products from '../data/Data';
 
 const SearchPage: NextPage = () => {
   const [query, setQuery] = useState(true);
-  useEffect(()=>{
-    setQuery(false)
-  },[query])
+  useEffect(() => {
+    setQuery(false);
+  }, [query]);
   if (query) return null;
-  return <div>Searching</div>;
+  return (
+    <div>
+      <ProductsList products={products} />
+    </div>
+  );
 };
 
 export default SearchPage;
