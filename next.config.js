dfa8b71e018ca476caf4fa9c5fc7ef23ef27caf7
riad <riad@gmail.com>
2022-08-24 15:18:25 -0400

    problem in state management bcause of redirection

--- next.config.js
+++ next.config.js
@@ -7,15 +7,15 @@ const nextConfig = {
       'https://rukminim1.flixcart.com/image/416/416/kq18n0w0/mobile/d/l/f/narzo-30-5g-rmx3242-realme-original-imag45yjdkgxddvh.jpeg?q=70',
     ],
   },
-  async redirects() {
-    return [
-      {
-        source: '/payment',
-        destination: '/payment/personalInfo',
-        permanent: true,
-      },
-    ]
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/payment',
+  //       destination: '/payment/personalInfo',
+  //       permanent: true,
+  //     },
+  //   ]
+  // },
 };
 
 module.exports = nextConfig;
