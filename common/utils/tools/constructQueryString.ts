56b393b9d83162e5d11332036521d3e9da9dbde1
riad <riad@gmail.com>
2022-08-31 16:43:42 -0400

    ++

--- common/utils/tools/constructQueryString.ts
+++ common/utils/tools/constructQueryString.ts
@@ -1,10 +1,15 @@
+import { URLSearchParams } from "url";
 import { Filter } from "../../types/@appTypes";
 
 function constructQueryString(filter:Filter) {
-      
-    const params = Object.keys(filter).filter((param) => filter[param]);
-    const queryString = params.reduce((a, b) => `${a}&${b}=${encodeURIComponent(filter[b])}`, '');
-    return queryString.slice(2)
+ 
+    // const params = Object.keys(filter).filter((param) => filter[param]);
+    // const queryString = params.reduce((a, b) => `${a}&${b}=${encodeURIComponent(filter[b][i)}`, '');
+    // return queryString.slice(2)
+    const queryParams=new URLSearchParams(filter);
+    const queryString=queryParams.toString();
+    return queryString
+  
 }
 
 export default constructQueryString
