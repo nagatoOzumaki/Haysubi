47e786aca7585c1caf63121a14bbf920e8755db3
riad <riad@gmail.com>
2022-08-31 16:55:32 -0400

    ++

--- common/utils/tools/constructQueryString.ts
+++ common/utils/tools/constructQueryString.ts
@@ -1,4 +1,4 @@
-import { URLSearchParams } from "url";
+
 import { Filter } from "../../types/@appTypes";
 
 function constructQueryString(filter:Filter) {
@@ -6,9 +6,9 @@ function constructQueryString(filter:Filter) {
     // const params = Object.keys(filter).filter((param) => filter[param]);
     // const queryString = params.reduce((a, b) => `${a}&${b}=${encodeURIComponent(filter[b][i)}`, '');
     // return queryString.slice(2)
-    const queryParams=new URLSearchParams(filter);
-    const queryString=queryParams.toString();
-    return queryString
+    // const queryParams=new URLSearchParams(filter);
+    // const queryString=queryParams.toString();
+    return "queryString"
   
 }
 
